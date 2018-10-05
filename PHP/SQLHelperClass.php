1ec5adf0b15099efe9405c2cdf166cc2b9cd7671
Andrew H <FlamesRunner@users.noreply.github.com>
2018-10-05 10:51:56 -0400

    Made some fixes
    Moved DSN to a single line as it is a singly-used variable, removed an un-needed return statement

--- PHP/SQLHelperClass.php
+++ PHP/SQLHelperClass.php
@@ -24,20 +24,17 @@
         private $dbPass;
         private $dbChar;
         private $options;
-        private $dsn;
         private $pdoObject;
-        public function __construct($dbName, $dbHost, $dbUser, $dbPass = "", $dbChar, $options) {
+        public function __construct($dbName, $dbHost, $dbUser, $dbPass, $dbChar, $options) {
             $this->dbName = $dbName;
             $this->dbHost = $dbHost;
             $this->dbUser = $dbUser;
             $this->dbPass = $dbPass;
             $this->dbChar = $dbChar;
-            $this->options = $optionsArray;
-            $this->dsn = "mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName . ";charset=" . $this->dbChar;
+            $this->options = $options;
             try {
-                $PDO = new PDO($this->dsn, $this->dbUser, $this->dbPass, $this->options);
+                $PDO = new PDO("mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName . ";charset=" . $this->dbChar, $this->dbUser, $this->dbPass, $this->options);
                 $this->pdoObject = $PDO;
-                return true;
             } catch (PDOException $error) {
                 $this->pdoObject = null;
                 throw new Exception("Could not establish a connection to the database, exception: " . $error->getMessage());
